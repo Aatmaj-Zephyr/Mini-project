70317ccdfa419f387264ed96e9ab953ee65fc0b1
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-02-05 13:53:44 +0530

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,13 @@
 var staticCacheName = "pwa";
- 
+const assetsToCache = [
+  '/index.html',
+  'mystyle.css'
+];
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      console.log("Service worker installed");
+      return cache.addAll([assetsToCache]);
     }) 
   );
 });
@@ -13,6 +17,7 @@ self.addEventListener("fetch", function (event) {
  
   event.respondWith(
     caches.match(event.request).then(function (response) {
+      console.log("Service worker Fetched");
       return response || fetch(event.request);
     })
   );
