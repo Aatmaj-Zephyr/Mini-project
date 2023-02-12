9eebbb53496fa23c20e4bcc6c8c79f387d4c3664
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-02-12 10:59:45 +0530

    update index.html and pushapi.js

--- src/js/pushapi.js
+++ src/js/pushapi.js
@@ -15,3 +15,31 @@ if ('Notification' in window) {
           enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission); 
       } 
 } 
+const displayConfirmNotification = () => { 
+    if ('serviceWorker' in navigator) { 
+       const options = {
+             body: 'You successfully subscribed to our Notification service!',
+             icon: 'src/images/icons/app-icon-96x96.png',
+             image: 'src/images/main-image-sm.jpg',
+             dir: 'ltr',
+             lang: 'en-US',
+             badge: 'src/images/icons/app-icon-96x96.png',
+             tag: 'confirm-notification',
+             actions: [ 
+                {
+                     action: 'confirm',
+                     title: 'Okay',
+                     icon: 'src/images/icons/app-icon-96x96.png' 
+                 }, 
+                 {
+                     action: 'cancel',
+                     title: 'Cancel',
+                     icon: 'src/images/icons/app-icon-96x96.png' 
+                   
+                 } 
+             ] 
+      }; 
+      navigator.serviceWorker.ready 
+        .then(sw => sw.showNotification('Successfully subscribed!', options));
+    } 
+}; 
