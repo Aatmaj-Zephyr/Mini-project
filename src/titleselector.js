379452f2c5f074241efadb763540130ae659b7df
Aatmaj <Aatmaj.mhatre@gmail.com>
2023-04-12 14:10:34 +0530

    Create titleselector.js

---
+++ src/titleselector.js
@@ -0,0 +1,50 @@
+// Get references to the input and list elements
+const input = document.getElementById("name")
+const list = document.getElementById("ttmenu")
+console.log(input)
+console.log(list)
+// Load the options from a text file
+fetch('title.txt')
+	.then(response => response.text())
+	.then(text => {
+		// Split the text into an array of options
+		const options = text.split('\n');
+		
+		// Add each option to the list
+		options.forEach(option => {
+			const li = document.createElement('li');
+			li.classList.add('dropdown-item');
+			li.textContent = option;
+			// Add click event listener to select option
+			li.addEventListener('click', () => {
+				input.value = option;
+				list.style.display = 'none';
+			});
+			list.appendChild(li);
+		});
+	})
+	.catch(error => console.error(error));
+
+// Add event listener for input changes
+input.addEventListener('input', () => {
+    console.log("input changed  ")
+	// Get the search term and filter the options
+	const term = input.value.toLowerCase();
+	const items = list.querySelectorAll('.dropdown-item');
+	console.log(items);
+	items.forEach(item => {
+		const text = item.textContent.toLowerCase();
+		if (text.indexOf(term) === -1) {
+			item.style.display = 'none';
+		} else {
+			item.style.display = 'block';
+		}
+	});
+});
+
+// Add event listener for click to show the search bar
+input.addEventListener('click', () => {
+	//If the user clicks on the input element, display the list element
+	list.style.display = 'block';
+	
+});
